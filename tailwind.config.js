6bb58767f46f018d71505708beb3e0d590ac7a5b
yosin <umnovanika1@gmail.com>
2024-04-28 10:52:30 +0500

    adding some thing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/*.{html,css}"],
   theme: {
+    screens: {
+      sm: "320px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+    },
     extend: {},
   },
   plugins: [],
